fc537c579d500b8a21aabeadc7623504f68a93fe
Anish Karim <canishk@gmail.com>
2026-06-10 20:23:21 +0530

    PWA files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { registerSW } from 'virtual:pwa-register';
+import './index.css';
+import App from './App.jsx';
 
-import { PlayerProvider } from './context/PlayerContext.jsx'
+import { PlayerProvider } from './context/PlayerContext.jsx';
+registerSW(
+  {
+    immediate: true,
+    onRegisteredSW(swUrl, registration) {
+      console.log('SW registered: ', swUrl);
+    },
+  }
+);
 
 ReactDOM.createRoot(
   document.getElementById('root')
